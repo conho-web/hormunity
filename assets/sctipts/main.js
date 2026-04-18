49cbe67b2658016016e9d048ecff5995e532688b
conho-web <conho@internet.ru>
2026-04-18 10:47:16 +0300

    feat: section products

--- assets/sctipts/main.js
+++ assets/sctipts/main.js
@@ -21,3 +21,54 @@ if (burger && modal) {
     });
   }
 }
+
+const swiper = new Swiper('.mySwiper', {
+  grabCursor: true,
+  initialSlide: 2,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  loop: false,
+  effect: "creative",
+  creativeEffect: {
+    limitProgress: 2,
+    prev: {
+      translate: ["-115%", 0, -200], 
+      scale: 0.9,
+    },
+    next: {
+      translate: ["115%", 0, -200],
+      scale: 0.9,
+    },
+  },
+  breakpoints: {
+    320: {
+      creativeEffect: {
+        prev: {
+          translate: ["-105%", 0, -200],
+          scale: 0.85,
+        },
+        next: {
+          translate: ["105%", 0, -200],
+          scale: 0.85,
+        },
+      },
+    },
+    468: {
+      creativeEffect: {
+        limitProgress: 2,
+        prev: {
+          translate: ["-115%", 0, -200], 
+          scale: 0.9,
+        },
+        next: {
+          translate: ["115%", 0, -200],
+          scale: 0.9,
+        },
+      },
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
